3a3a722caeb96184a01993457547934416e864ac
jithintpanicker <56878450+jithintpanicker@users.noreply.github.com>
2019-10-31 15:33:38 +0530

    Add files via upload

---
+++ Thirty_Days_of_Code/Day0.java
@@ -0,0 +1,19 @@
+package Thirty_Days_of_Code;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Day0 {
+	public static void main(String[] args) {
+        
+		String inputString ="Welcome to 30 Days of Code!";
+
+		System.out.println("Hello, World.");
+        System.out.println(inputString);
+      
+	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
+	}
+}
+

0c433e9f3914740eac2879ee4e88271c3920ad9b
jithintpanicker <56878450+jithintpanicker@users.noreply.github.com>
2019-10-31 13:14:08 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,33 @@
+package jithi;
+
+public class condition {
+	
+	int i,j,k;
+
+	public void process()
+	{
+//	if(i>j)
+//	{
+//		if(i==7)
+//		{
+//			System.out.println("7 entered");
+//		}
+//		System.out.println("the geatest value is" +i);
+//	}
+//	else if(i==j)
+//	
+//		System.out.println("i is equal to j");
+//
+//	
+//	else
+//		
+//	
+//		System.out.println("the geatest value is" +j);
+//	
+//	
+		String a=(i < j) ? "greatest is" +j : "greatest is" +i;
+		
+		System.out.println(a);
+		
+	}
+}
